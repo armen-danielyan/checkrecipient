b5483341c437d27d13edaa7c0cfa4a4deece738f
Armen <danielyan_armen@yahoo.com>
2016-01-21 16:01:02 +0400

    update

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,4 +1,5 @@
 var gmail = null,
+    recipientList = [];
 gMailManager = {
 
     eventInitialized: false,
@@ -25,7 +26,7 @@ gMailManager = {
                 id: "check-recipient-xt-modal",
                 role: "dialog"
             }),
-            $sendButton = $(".T-I.J-J5-Ji.aoO.T-I-atl.L3")
+            $sendButton = $(".T-I.J-J5-Ji.aoO.T-I-atl.L3"),
             $modalDialog = $("<div/>", {class: "modal-dialog"}),
             $modalContent = $("<div/>", {class: "modal-content"}),
             $modalHeader = $("<div/>", {class: "modal-header"}).append(
@@ -62,13 +63,6 @@ gMailManager = {
             $modalSubjectLabel = $("<label/>", {
                 for: "check-recipient-subject"
             }).text("Subject"),
-            $modalAutocompleteInput = $("<input/>", {
-                class: "form-control",
-                id: "check-recipient-autocomplete"
-            }),
-            $modalAutocompleteLabel = $("<label/>", {
-                for: "check-recipient-autocomplete"
-            }).text("Autocomplete"),
             $modalFooter = $("<div/>", {class: "modal-footer"}),
             $modalBackButton = $("<button/>", {
                 class: "btn btn-default",
@@ -82,13 +76,29 @@ gMailManager = {
         $("<div/>", {class: "form-group"}).append($modalTitleLabel, $modalTitleInput).appendTo($modalBody);
         $("<div/>", {class: "form-group"}).append($modalEmailLabel, $modalEmailinput).appendTo($modalBody);
         $("<div/>", {class: "form-group"}).append($modalSubjectLabel, $modalSubjectInput).appendTo($modalBody);
-        $("<div/>", {class: "form-group"}).append($modalAutocompleteLabel, $modalAutocompleteInput).appendTo($modalBody);
 
         $modalFooter.append($modalBackButton, $modalSendButton);
         $modalContent.append($modalHeader, $modalBody, $modalFooter).appendTo($modalDialog);
         $modal.append($modalDialog).appendTo($body);
 
         $modalSendButton.click(function () {
+            //console.log(jsonBuilder(recipientList));
+            var jsonData = jsonBuilder(recipientList);
+            $.ajax({
+                dataType: "json",
+                type: "POST",
+                data: jsonData,
+                contentType: "application/json",
+                url: "https://quiverlive.getcheckrecipient.com/api_external/check_email",
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function () {
+                    console.log("ERROR");
+                }
+
+            });
+
             $sendButton.click();
             $modalBackButton.click();
         })
@@ -120,23 +130,78 @@ gMailManager = {
             $sendButton.hide();
             $newSendButton.appendTo($sendButton.parent());
             $newSendButton.click(function () {
+
                 var $parentTable = $(this).parents("table.aoP.aoC"),
                     $email = $($parentTable.find("form table.GS div.vR div.vT")[0]),
                     $subject = $parentTable.find("form div.aoD.az6 input[name='subjectbox']"),
                     email = ($email) ? $email.text() : "",
                     subject = $subject.val();
 
-                var autocompleteList = [];
-                $("div.ah.aiv.aJS div.am").each(function(){
-                    autocompleteList.push($(this).text());
-                });
-
                 $("#check-recipient-xt-modal #check-recipient-address").val(email);
                 $("#check-recipient-xt-modal #check-recipient-subject").val(subject);
-                $("#check-recipient-xt-modal #check-recipient-autocomplete").val(autocompleteList.join(", "));
+                //$("#check-recipient-xt-modal #check-recipient-autocomplete").val(autocompleteListStr);
                 $("#check-recipient-xt-modal").modal();
                 $("#check-recipient-xt-modal #modal-send").prop("disabled", true);
                 chrome.extension.sendMessage({msg: "api-call"});
+            });
+
+            var recipient = {},
+                recipientAddress = "",
+                similarRecipient = {},
+                similarRecipientList = [];
+
+            $("div.wO").on("DOMNodeInserted", function (e) {
+
+                var matchLength = $(this).children(".vO").val().length;
+
+                if ($(e.target).is("div.vR")) {
+
+                    recipientAddress = $(e.target).find(".vN").attr("email");
+                    var weight = 0;
+
+                    $("div.ah.aiv.aJS div.am").each(function () {
+                        if ($(this).children("div.ao5").length > 0) {
+                            similarRecipient["name"] = $(this).children(".ao5").text();
+                            similarRecipient["email"] = $(this).children(".Sr").text();
+                        } else {
+                            similarRecipient["name"] = "";
+                            similarRecipient["email"] = $(this).text();
+                        }
+                        similarRecipient["email_name"] = "";
+                        similarRecipient["email_domain"] = "";
+                        similarRecipient["weight"] = ++weight;
+                        similarRecipient["address_match_length"] = matchLength;
+                        similarRecipient["name_match_length"] = matchLength;
+
+                        similarRecipientList.push(similarRecipient);
+                        similarRecipient = {};
+                    });
+
+                    recipient["address"] = recipientAddress;
+                    recipient["name"] = "";
+                    recipient["email_name"] = "";
+                    recipient["email_domain"] = "";
+                    recipient["recipient_type"] = 1;
+                    recipient["weight"] = 14848;
+                    recipient["similar_addresses"] = similarRecipientList;
+                    recipient["similar_addresses_new_str"] = "";
+                    recipient["similar_addresses_new"] = "";
+
+                    recipientList[recipientAddress] = recipient;
+                    similarRecipientList = [];
+                    recipient = {};
+
+                }
+                //console.log(recipientList);
+            });
+
+            $("div.wO").on("DOMNodeRemoved", function (e) {
+
+                if ($(e.target).is("div.vR")) {
+                    recipientAddress = $(e.target).find(".vN").attr("email");
+                    delete recipientList[recipientAddress];
+                }
+                //console.log(recipientList);
             });
         }
     },
@@ -147,35 +212,67 @@ gMailManager = {
     }
 };
 
-$(document).ready(function () {
-    console.log("Hello");
+$(window).hashchange(function () {
+    var hashcode = window.location.hash;
+    if (hashcode.indexOf("?compose=") > -1) {
+        gMailManager.addSendButtonEventHandler();
+        console.log("Link bar timer logging ....");
+    } else {
+        gMailManager.eventInitialized = false;
+    }
+});
 
-    if (window.location.hash.indexOf("?compose=") > -1) {
-        var sendButtonTimer = setInterval(function () {
-            if ($("div.AD table.iN .aoX").length > 0) {
-                gMailManager.addSendButtonEventHandler();
-                console.log("Link bar timer logging....");
-                clearInterval(sendButtonTimer);
-            }
-        }, 500);
+$('html').bind('keypress', function (e) {
+    if (e.keyCode === 13 && e.ctrlKey) {
+        return false;
+    }
+});
+
+function jsonBuilder(recipientsData) {
+
+    var recipients = [],
+        tmpRecipient,
+        weight = 0;
+
+    for(var recipientData in recipientsData){
+        tmpRecipient = recipientsData[recipientData];
+        tmpRecipient["weight"] = ++weight;
+        recipients.push(tmpRecipient);
     }
 
-    $(window).hashchange(function () {
-        var hashcode = window.location.hash;
-        if (hashcode.indexOf("?compose=") > -1) {
-            gMailManager.addSendButtonEventHandler();
-        } else {
-            gMailManager.eventInitialized = false;
-        }
-    });
+    var jsonObj = {
+        cr_action: "check_email",
+        api_token: "b1b6f6938c96e3be0e42de3d61777015",
+        username: "tom@quiversoftware.com",
+        rb_username: "",
+        user_domain: "quiversoftware.com",
+        version: "1.5.1.9",
+        device: "gmail",
+        recipients: recipients,
+        rb_recipients: [
+            "tomadams787@gmail.com",
+            "tim@checkrecipient.com"
+        ],
+        reply_to: " <7F651A1EJSIFF428B3239A37DF227959C544469@ldsexchange01.thoj.local>",
+        num_attachments: 0,
+        size_attachments: "0.0",
+        sandpit_client: false,
+        rules: "",
+        email_type: "new_email",
+        attachment_extensions: "",
+        has_attachment: true,
+        attachments: [{
+            name: "Test document",
+            extension: ".doc",
+            attachment_words: ""
+        }],
+        subject_words: "",
+        regex_filters_return: {}
+    };
 
-    $('html').bind('keypress', function (e) {
-        if (e.keyCode === 13 && e.ctrlKey) {
-            return false;
-        }
-    });
-
-});
+    var jsonStr = JSON.stringify(jsonObj);
+    return jsonStr;
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.msg) {
